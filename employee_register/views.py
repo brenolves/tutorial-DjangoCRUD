4c9c0e31cfb1cc6089754ae1c27cd10a75f3c290
brenolves <brenoalves43@hotmail.com>
2022-04-30 11:37:15 -0300

    Projeto inicializado

---
+++ employee_register/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def employee_list(request):
+    return
+
+def employee_form(request):
+    return HttpResponse('asdasda')
+
+def employee_delete(request):
+    return
